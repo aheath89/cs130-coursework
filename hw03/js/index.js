0696a64a775a39955813d54e5ae07dfa15037aee
Abby Heath <abbyheath@Abbys-MacBook-Pro-2.local>
2021-05-05 15:04:24 -0500

    completed hw03 files and adding hw03 to homepage

---
+++ hw03/js/index.js
@@ -0,0 +1,79 @@
+/**
+ * 
+ * -------------------------------------
+ * DOM Manipulation / Traversal Activity
+ * -------------------------------------
+ * 
+ * 1. Create and attach an event handler (function) to each ".image" 
+ * element so that when the ".image" element is clicked, the corresponding 
+ * image loads in the .featured image element.
+ * 
+ * 2. Create event handlers for the next and previous buttons. The next button should
+ *    show the next image in the thumbnail list. The previous should show the previous.
+ * 
+ * 3. If you get to the end, start at the beginning. 
+ * 
+ * 4. If you get to the beginning, loop around to the end.
+ * 
+ * 
+ */
+
+let currentIndex = 0;
+
+const images = [
+    'images/field1.jpg',
+    'images/purple.jpg',
+    'images/jar.jpg',
+    'images/green.jpg',
+    'images/green1.jpg',
+    'images/purple1.jpg',
+    'images/magnolias.jpg',
+    'images/daisy1.jpg'
+];
+
+const initScreen = () => {
+    images.forEach((image, idx) => {
+        document.querySelector('.cards').innerHTML += `
+            <li class="card">
+                <div class="image" 
+                    style="background-image:url('${image}')"
+                    data-index="${idx}"></div>
+            </li>`;
+    });
+};
+
+initScreen();
+
+const showImage = (ev) => {
+    const elem = ev.currentTarget;
+    document.querySelector('.featured_image').style.backgroundImage = elem.style.backgroundImage;
+    currentIndex = parseInt(elem.dataset.index);
+};
+
+const showNext = () => {
+    currentIndex += 1;
+    if (currentIndex == 8) {
+        currentIndex = 0;
+    }
+    document.querySelector('.featured_image').style.backgroundImage = document.querySelectorAll('.image')[currentIndex].style.backgroundImage;
+};
+
+const showPrev = () => {
+    currentIndex -= 1;
+    if (currentIndex == -1) {
+        currentIndex = 7;
+    }
+    document.querySelector('.featured_image').style.backgroundImage = document.querySelectorAll('.image')[currentIndex].style.backgroundImage;
+};
+
+
+// attach event handler to all of the image tags 
+// (after initScreen() has been invoked).
+const imageElements = document.querySelectorAll('.image');
+for (const elem of imageElements) {
+    elem.onclick = showImage;
+}
+
+document.querySelector('.next').onclick = showNext;
+document.querySelector('.prev').onclick = showPrev;
+document.querySelector('.featured_image').onclick = showNext;
